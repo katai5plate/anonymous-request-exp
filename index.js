c3a0fb20be32418314b4a9f92e1fd1f34198e2ca
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2019-07-28 15:45:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const req = require('request');
 
 const ipifyURL = 'https://api.ipify.org';
 
-tor.request(ipifyURL, (err, res, body) => {
-  if (!err && res.statusCode == 200) {
-    console.log(`Your Anonymous IP is ${body}`);
-  }
-});
-
-req.get(ipifyURL, (err, res, body) => {
-  if (!err && res.statusCode == 200) {
-    console.log(`Your Original IP is ${body}`);
-  }
+[
+  { cli: req, mes: body => `Your Original IP is ${body}` },
+  { cli: tor, mes: body => `Your Anonymous IP is ${body}` }
+].forEach(({cli, mes}) => {
+  cli.request(ipifyURL, (err, res, body) => {
+    if (!err && res.statusCode == 200) {
+      console.log(mes(body));
+    }
+  })
 });
